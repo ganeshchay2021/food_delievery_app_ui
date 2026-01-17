b71f86cc4470ae4572d5b23f24ac65e2beea15cd
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-01-17 17:10:18 +0545

    Feat(Model): Models are added

---
+++ lib/models/category_model.dart
@@ -0,0 +1,20 @@
+class Category {
+  String image, name;
+
+  Category({required this.image, required this.name});
+}
+
+List<Category> myCategories = [
+  Category(
+    image: 'assets/images/burger.png',
+    name: 'Burger',
+  ),
+  Category(
+    image: 'assets/images/pizza (1).png',
+    name: 'Pizza',
+  ),
+  Category(
+    image: 'assets/images/cup cake.png',
+    name: 'Cup Cake',
+  ),
+];
